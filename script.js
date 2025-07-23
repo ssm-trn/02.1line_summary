feb8a4cfcc93953a6bf683cf6e545cea96522415
ssm-trn <susumu.dct21@gmail.com>
2025-07-23 21:00:11 +0900

    Updates

--- script.js
+++ script.js
@@ -30,10 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
         
         try {
             // バックエンドAPIを呼び出し
-            const apiUrl = 'https://02-1line-summary-5mvqf5hn5-ssm-trns-projects.vercel.app/api/summarize';
+            const apiUrl = 'https://02-1line-summary-5mvqf5hn5-ssm-trns-projects.vercel.app/api/proxy';
             const response = await fetch(apiUrl, {
-              mode: 'cors',
-              credentials: 'include',
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
